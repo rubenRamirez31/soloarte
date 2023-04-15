470c485ac8dae554b7dd62c7561ba2d226f30b3e
rubenRamirez31 <L20TE0221@teziutlan.tecnm.mx>
2023-04-14 20:51:04 -0600

    se agrego un ejemplo para llevar a cabo la validacion del login falta hacer la correcta implementacion

--- Paginas/login.php
+++ Paginas/login.php
@@ -30,3 +30,74 @@
 </body>
 
 </html>
+
+
+<!-- LOGICA PARA EJECUTAR EL LOGIN -->
+
+<?php
+//Incluye el header
+require '../Conection/cn.php';
+
+// //conexion a la base de datos
+// require 'includes/config/database.php';
+
+// $db = conectarDB();
+
+// $errores = [];
+
+// //Autenticar el usuario
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+//     $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+
+//     $password = mysqli_real_escape_string($db, $_POST['password']);
+
+//     if (!$email) {
+
+//         $errores[] = "El Email es obligatorio o no es valido";
+//     }
+
+//     if (!$password) {
+//         $errores[] = "El Password es obligatorio";
+//     }
+
+//     if (empty($errores)) { //validar el usuario
+
+//         //revisar si el usuario existe
+
+//         $query = "SELECT * FROM usuarios WHERE email = '${email}'";
+//         $resultado = mysqli_query($db, $query);
+
+
+
+//         if ($resultado->num_rows) {
+
+//             //Revisar si el password es correcto
+//             $usuario = mysqli_fetch_assoc($resultado);
+//             // var_dump($usuario);
+
+//             //Verificar si el password es correcto o no
+
+//             $auth = password_verify($password, $usuario['password']);
+
+//             // var_dump($auth);
+
+//             if ($auth) {
+//                 // El usuario esta autenticado
+//                 session_start();
+
+//                 //LLenar el arreglo de la sesion
+//                 $_SESSION['usuario'] = $usuario['email'];
+//                 $_SESSION['login'] = true;
+
+//                 header('location: /admin');
+
+//             } else {
+
+//                 $errores[] = "La contraseña es incorrecta";
+//             }
+//         } else {
+//             $errores[] = "El usuario no existe";
+//         }
+//     }
+// }
