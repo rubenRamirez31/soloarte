6f9533d6b858e602887711899b037e71012760c9
rubenRamirez31 <L20TE0221@teziutlan.tecnm.mx>
2023-05-06 18:34:46 -0600

    cambios

--- Modales/actualizarModal.php
+++ Modales/actualizarModal.php
@@ -1,13 +1,16 @@
 <!-- Modal -->
-<div class="modal fade" id="agregarModal" tabindex="-1" aria-labelledby="agregarModalLabel" aria-hidden="true">
+<div class="modal fade" id="actualizarModal" tabindex="-1" aria-labelledby="actualizarModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="agregarModalLabel">Agregar Producto</h1>
+                <h1 class="modal-title fs-5" id="actualizarModalLabel">Actualizar Producto</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="../../Transacciones/agregar.php" method="POST" enctype="multipart/form-data">
+
+                <form action="../../Transacciones/actualizar.php" method="POST" enctype="multipart/form-data">
+                    <input type="hidden" id="id" name="id">
+
                     <div class="mb-3">
                         <label for="nombre" class="form-label">Nombre: </label>
                         <input type="text" name="nombre" id="nombre" class="form-control" required>
@@ -29,13 +32,13 @@
                     </div>
 
                     <div class="mb-3">
-                        <p class="text text-danger text-center text-uppercase" style="font-weight: bold;">Las imagenes se podrán agregar una vez creado el producto</p>
+                        <p class="text text-danger text-center text-uppercase" style="font-weight: bold;">Las imagenes se editan en su apartado</p>
                     </div>
 
                     <div class="row justify-content-end">
                         <div style="display: flex; justify-content: end;">
                             <button type="button" class="btn btn-danger" style="margin-right: 5px;" data-bs-dismiss="modal"><i class="fa-solid fa-xmark" style="margin-right: 2px;"></i>Cancelar</button>
-                            <button type="submit" class="btn btn-success"><i class="fa-regular fa-floppy-disk" style="margin-right: 2px;"></i> Agregar</button>
+                            <button type="submit" class="btn btn-success"><i class="fa-regular fa-floppy-disk" style="margin-right: 2px;"></i> Confirmar</button>
                         </div>
 
                     </div>
--- Paginas/Admin/index.php
+++ Paginas/Admin/index.php
@@ -11,18 +11,40 @@
 
 <body>
     <?php include '../../Layout/navbar.php' ?>
+    <?php
 
-    <?php if (isset($_GET["resul"])  === "1") : ?>
+    $resultado = $_GET['resul'] ?? null;
 
+    if ($resultado === "1") : ?>
         <script>
             Swal.fire(
-                'Producto Agregado Correctamente',
+                'Producto Creado Correctamente',
                 '',
                 'success'
             );
         </script>
 
-    <?php endif ?>
+    <?php elseif ($resultado === "2") : ?>
+
+        <script>
+            Swal.fire(
+                'Producto Actualizado Correctamente',
+                '',
+                'success'
+            );
+        </script>
+
+    <?php elseif ($resultado === "3") : ?>
+
+        <script>
+            Swal.fire(
+                'Producto Eliminado Correctamente',
+                '',
+                'success'
+            );
+        </script>
+
+    <?php endif;  ?>
 
 
 
@@ -34,21 +56,20 @@
     require '../../Conection/cn.php';
 
     $query = "SELECT * FROM productos";
-
     $productos = $db->query($query);
 
-
     ?>
-
-
-
     <div class="container py-3">
         <h1 class="text text-center">Productos</h1>
 
 
-        <div class="row justify-content-end">
+        <div class="row justify-content-between">
             <div class="col-auto">
-                <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#agregarModal"><i class="fa-solid fa-circle-plus mr-1" style="margin-right: 5px;"></i>Nuevo Registro</a>
+                <label for="campo">Buscar</label>
+                <input type="text" class=" form-control" id="campo" name="campo">
+            </div>
+            <div class="col-auto d-flex align-items-end">
+                <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#agregarModal"><i class="fa-solid fa-circle-plus mr-1" style="margin-right: 5px;"></i>Nuevo Producto</a>
             </div>
         </div>
 
@@ -59,10 +80,10 @@
                     <th>Id</th>
                     <th>Nombre</th>
                     <th>Imagen</th>
-                    <th>Descripcion</th>
+                    <th>Descripción</th>
                     <th>Precio</th>
                     <th>Stock</th>
-                    <th>Accion</th>
+                    <th class="text text-center">Acción</th>
                 </tr>
             </thead>
             <tbody>
@@ -75,7 +96,17 @@
                         <td> <?php echo $row_producto['descripcion'] ?> </td>
                         <td> <?php echo $row_producto['precio'] ?> </td>
                         <td> <?php echo $row_producto['stock'] ?> </td>
-                        <td> --- </td>
+                        <td class=" d-lg-flex justify-content-around">
+                            <div>
+                                <a href="#" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#actualizarModal" data-bs-id="<?php echo $row_producto['id_producto'] ?>">
+                                    <i class="fa-solid fa-pen-to-square"></i> Actualizar</a>
+
+                            </div>
+                            <div>
+                                <a href="#" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#eliminarModal" data-bs-id="<?php echo $row_producto['id_producto'] ?>"><i class="fa-solid fa-trash"></i> Eliminar</a>
+
+                            </div>
+                        </td>
                     </tr>
 
                 <?php endwhile ?>
@@ -94,7 +125,57 @@
         }
     </script>
 
+
+
     <?php include '../../Modales/agregarModal.php'; ?>
+    <?php include '../../Modales/actualizarModal.php'; ?>
+    <?php include '../../Modales/EliminarModal.php'; ?>
+
+
+
+    <script>
+        let actualizarModal = document.getElementById('actualizarModal');
+        let eliminarModal = document.getElementById('eliminarModal');
+
+
+        actualizarModal.addEventListener('shown.bs.modal', event => {
+            let button = event.relatedTarget;
+            let id = button.getAttribute('data-bs-id');
+
+            let inputId = actualizarModal.querySelector('.modal-body #id');
+            let inputNombre = actualizarModal.querySelector('.modal-body #nombre');
+            let inputDescripcion = actualizarModal.querySelector('.modal-body #descripcion');
+            let inputPrecio = actualizarModal.querySelector('.modal-body #precio');
+            let inputStock = actualizarModal.querySelector('.modal-body #stock');
+
+            let url = "../../Transacciones/getProductos.php";
+
+            let formData = new FormData();
+            formData.append('id', id);
+
+            fetch(url, {
+                    method: "POST",
+                    body: formData
+                }).then(response => response.json())
+                .then(data => {
+                    inputId.value = data.id_producto
+                    inputNombre.value = data.nombre
+                    inputDescripcion.value = data.descripcion
+                    inputPrecio.value = data.precio
+                    inputStock.value = data.stock
+
+                }).catch(err => console.log(err))
+        });
+
+        eliminarModal.addEventListener('shown.bs.modal', event => {
+            let button = event.relatedTarget;
+            let id = button.getAttribute('data-bs-id');
+
+            eliminarModal.querySelector('.modal-footer #id').value = id;
+
+        });
+    </script>
+
     <?php include '../../Layout/scripts.php' ?>
 </body>
 
