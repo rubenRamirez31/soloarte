79ab6d07d6842990cd89fb601b3e00700bbcbff6
rubenRamirez31 <l20te0221@teziutlan.tecnm.mx>
2023-05-16 12:44:58 -0600

    Se agrega validacion

--- Transacciones/actualizar.php
+++ Transacciones/actualizar.php
@@ -1,21 +1,24 @@
-<?php 
-require '../Conection/cn.php';
+<?php
 
-$id = $db->real_escape_string($_POST['id']);
-$nombre = $db->real_escape_string($_POST['nombre']) ;
-$descripcion = $db->real_escape_string($_POST['descripcion']) ;
-$precio = $db->real_escape_string($_POST['precio']);
-$stock = $db->real_escape_string($_POST['stock']);
+if ($_SERVER[$_POST]) {
+    require '../Conection/cn.php';
+
+    $id = $db->real_escape_string($_POST['id']);
+    $nombre = $db->real_escape_string($_POST['nombre']);
+    $descripcion = $db->real_escape_string($_POST['descripcion']);
+    $precio = $db->real_escape_string($_POST['precio']);
+    $stock = $db->real_escape_string($_POST['stock']);
 
 
-$query = "UPDATE  productos SET nombre = '$nombre', descripcion = '$descripcion', precio = $precio, stock = $stock 
+    $query = "UPDATE  productos SET nombre = '$nombre', descripcion = '$descripcion', precio = $precio, stock = $stock 
 WHERE id_producto = $id ";
 
-if ($db->query($query)) {
-    header('Location:/soloarte/Paginas/Admin/index.php?resul=2');
+    if ($db->query($query)) {
+        header('Location:/soloarte/Paginas/Admin/index.php?resul=2');
+    } else {
+        echo 'error al insertar';
+        exit;
+    }
 } else {
-    echo 'error al insertar';
-    exit;
+    header('Location: /soloarte');
 }
-
-?>
