cba5407d8a6146d0710ed78f50f386f72496c0ba
MeendyM <l20te0178@teziutlan.tecnm.mx>
2023-05-15 20:52:55 -0600

    carrito

--- Paginas/Usuario/Productos.php
+++ Paginas/Usuario/Productos.php
@@ -14,53 +14,44 @@
 
     <?php include '../../Conection/cn.php' ?>
 
+    
+
     <div class="container mt-4">
 
         <h1 class="text text-center">Productos</h1>
 
-        <div class="card" style="width: 18rem;">
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="../../Images/CB1.jpg" class="d-block w-100 img-thumbnail" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="../../Images/CB2.jpg" class="d-block w-100 img-thumbnail" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="../../Images/CB3.jpg" class="d-block w-100 img-thumbnail" alt="...">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
+        
+ 
+<?php
+session_start();
+    $query = "select * from productos";
+    $productos = $db->query($query); 
+?>
 
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Vestibulum at eros</li>
-            </ul>
-            <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-            </div>
-        </div>
+<div class='row'>
+            <?php while ($row_productos = $productos->fetch_assoc()) :  ?>
+                <div class='col-4 mt-4'>
+                    <div class='card' style='width: 18rem;'>
+                    
+                        <ul class='list-group list-group-flush'>
+                        
+                            <li class='list-group-item'> <span style="font-weight: bold;"></span> <?php echo $row_productos['nombre'];?></li>
+                          
+                            <li class='list-group-item'> <span style="font-weight: bold;">Precio:</span> <?php echo $row_productos ['precio']; ?></li>
+                            <li class='list-group-item'> <span style="font-weight: bold;">En stock:</span> <?php echo $row_productos ['stock']; ?><span></span></li>
+                            <a href='..' class='btn btn-success'>Agregar al Carrito</a> 
+                            <a href='../Usuario/DetallesProducto.php?idProducto= $row_productos ["id_producto"]' class='btn btn-primary'>Mas Detalles</a> 
 
+
+                        </span> 
+                            
+                           
+                            
+                        </ul>
+                    </div>
+                </div>
+            <?php endwhile ?>
+        </div>    
 
     </div>
 
