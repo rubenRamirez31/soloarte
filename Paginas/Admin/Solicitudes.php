736a0ebedf06668aeefa735b894246a2a6db405f
rubenRamirez31 <20te0221@teziutlan.tecnm.mx>
2023-05-20 19:24:57 -0600

    se puede visualizar la solicitud del usuario

--- Paginas/Admin/Solicitudes.php
+++ Paginas/Admin/Solicitudes.php
@@ -1,20 +1,17 @@
-
 <?php
-    session_start();
+session_start();
 
-    if (!isset($_SESSION['usuario'])){
-        header("Location: NoAutenticado.php");
+if (!isset($_SESSION['usuario'])) {
+    header("Location: NoAutenticado.php");
+}
 
+if ($_SESSION['rol'] != 1) {
 
-    }
+    header("Location: NoAutorizado.php");
+}
 
-    if ($_SESSION['rol'] != 1) {
-
-        header("Location: NoAutorizado.php");
-
-    }
-       
- ?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -29,9 +26,102 @@
 
 <body>
     <?php include '../../Layout/navbar.php' ?>
-    <div class="container py-3">
+
+
+    <?php
+    include '../../Conection/cn.php';
+
+    // $query1 = "SELECT id_usuario FROM usuarios";
+    // $res1 = $db->query($query1);
+    // $datos = $res1->fetch_assoc();
+
+    $query2 = "SELECT id_usuario,fecha FROM solicitudes";
+    $res = $db->query($query2);
+    ?>
+
+
+    <div class="container mt-3">
         <h1 class="text text-center">Solicitudes</h1>
+
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div class=" col-lg-4 col-sm-12 mt-2">
+                    <h3 class="text text-center">Nuevas</h3>
+
+                    <div class=" table-responsive">
+                        <table id="snuevas" class="table table-sm table-striped table-hover mt-4 table-responsive">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th>Correo Electronico</th>
+                                    <th>Fecha</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <?php while ($row_solicitud = $res->fetch_assoc()) : ?>
+
+                                        <?php
+
+                                        $query1 = "SELECT email FROM usuarios WHERE id_usuario = " . $row_solicitud['id_usuario'];
+                                        $res1 = $db->query($query1);
+                                        $datos = $res1->fetch_assoc();
+                                        ?>
+
+
+                                        <td> <?php echo $datos['email'] ?> </td>
+                                        <td> <?php echo $row_solicitud['fecha'] ?> </td>
+                                        <td>
+                                           <a href="versoli.php?id=<?php echo $row_solicitud['id_usuario'] ?>" class="btn btn-outline-dark"><i class="fa-regular fa-eye"></i>Ver</a>
+                                        </td>
+                                    <?php endwhile ?>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+
+                <div class=" col-lg-4 col-sm-12  mt-2">
+                    <h3 class="text text-center">Vistas</h3>
+
+                </div>
+
+                <div class=" col-lg-4 col-sm-12  mt-2">
+                    <h3 class="text text-center">Aceptadas y rechazadas</h3>
+
+                </div>
+            </div>
+        </div>
     </div>
+    <script src="../../Jquery/jquery-3.6.4.min.js"></script>
+
+
+    <script>
+        var $j = jQuery.noConflict();
+
+        $j(document).ready(function() {
+            $j('#snuevas').DataTable({
+                "language": {
+                    "lengthMenu": "Mostrar _MENU_ Solicitudes",
+                    "zeroRecords": "Sin resultados",
+                    "info": "Mostrando solicitudes del _START_ al _END_ de un total de _TOTAL_ solicitudes",
+                    "infoEmpty": "Mostrando solicitudes del 0 al 0 de un total de 0 solicitudes",
+                    "infoFiltered": "(Filtrando de un total de _MAX_ solicitudes)",
+                    "sSearch": "Buscar",
+                    "oPaginate": {
+                        "sFirst": "Primero",
+                        "sLast": "Ultimo",
+                        "sNext": "Siguiente",
+                        "sPrevious": "Anterior"
+                    },
+                    "sProcessing": "Procesando..."
+                }
+            });
+        });
+    </script>
+
 
     <script type="text/javascript" src="../../DataTable/datatables.min.js"></script>
     <?php include '../../Layout/scripts.php' ?>
---
+++ Paginas/Admin/versoli.php
@@ -0,0 +1,126 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['usuario'])) {
+    header("Location: NoAutenticado.php");
+}
+
+if ($_SESSION['rol'] != 1) {
+
+    header("Location: NoAutorizado.php");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../DataTable/datatables.min.css">
+    <link rel="stylesheet" type="text/css" href="../../DataTable/DataTables-1.13.4/css/dataTables.bootstrap5.min.css">
+    <?php include '../../Layout/estilos.php'  ?>
+    <title>Solicitudes</title>
+</head>
+
+<body>
+    <?php include '../../Layout/navbar.php' ?>
+
+
+    <?php
+    include '../../Conection/cn.php';
+
+
+    $query2 = "SELECT * FROM solicitudes WHERE id_usuario = " . $_GET['id'];
+    $res = $db->query($query2);
+    $datos1 = $res->fetch_assoc();
+    ?>
+
+
+    <div class="d-flex justify-content-center">
+        <?php
+        $query = "SELECT email,nombre,apepat,apemat FROM usuarios WHERE id_usuario = " . $_GET['id'];
+        $res = $db->query($query);
+        $datos = $res->fetch_assoc();
+        ?>
+        <div class="col-lg-7 col-sm-12 mt-5">
+            <h1 class="text text-center">Solicitud</h1>
+            <h3 class="text text" style="font-weight: bold;"> Correo de usuario:</h3>
+            <h3 style="font-weight: none;"><?php echo $datos['email'] ?></h3>
+
+            <h3 class="text text" style="font-weight: bold;"> Nombre Completo:</h3>
+            <h3><?php echo $datos['nombre'] . " " . $datos['apepat'] . " " . $datos['apemat'] ?></h3>
+
+            <h3 class="text text" style="font-weight: bold;"> Fecha de creacion:</h3>
+            <h3><?php echo $datos1['fecha'] ?></h3>
+
+            <h2 class="text text-center">Respuestas</h2>
+
+            <div class="mb-3">
+                <h4>1. ¿Cuál es tu experiencia previa en la industria de distribución de artesanías o productos similares?</h4>
+                <h5><?php echo $datos1['P1'] ?></h5>
+            </div>
+
+            <div class="mb-3">
+                <h4 style="font-weight: bold;">2.¿Cuál es tu conocimiento sobre las distintas técnicas artesanales utilizadas en México?</h4>
+                <h5><?php echo $datos1['P2'] ?></h5>
+            </div>
+
+            <div class="mb-3">
+                <h4 style="font-weight: bold;">3. ¿Cuál es tu enfoque para garantizar que los productos sean auténticos y representen fielmente la cultura mexicana?</h4>
+                <h5><?php echo $datos1['P3'] ?></h5>
+            </div>
+
+            <div class="mb-3">
+                <h4 style="font-weight: bold;">4. ¿Cómo te mantendrías actualizado/a sobre las últimas tendencias y demandas en el mercado de las artesanías mexicanas?</h4>
+                <h5><?php echo $datos1['P4'] ?></h5>
+            </div>
+
+            <div class="mb-3">
+                <h4 style="font-weight: bold;">5. ¿Qué valor añadido crees que podrías aportar a la empresa en términos de asegurar la calidad de las artesanías y mejorar la experiencia del cliente?</h4>
+                <h4><?php echo $datos1['P5'] ?></h4>
+            </div>
+
+            <div class="mb-3">
+                <?php
+
+                $datos = array();
+                $id = $_GET['id'];
+                $sql = "SELECT imagenS FROM imgSolocitudes WHERE id_usuario=" . $_GET['id'] ;
+                $resul = $db->query($sql);
+
+                while ($row = $resul->fetch_assoc()) {
+                    $datos[] = $row['imagenS'];
+                }
+                ?>
+
+                <h4 style="font-weight: bold;">Imagenes</h4>
+
+                <?php
+                
+                for ($i=0; $i < 3; $i++) { 
+                    
+                    echo '<img style="width: 400px;" class=" img-fluid mx-2 mt-2" src="data:image/jpeg;base64,' . base64_encode($datos[$i]) . '" alt="Imagen del producto del usuario">'  ; 
+
+                }
+                
+                ?>
+
+            </div>
+
+
+
+        </div>
+
+
+
+
+
+    </div>
+    <script src="../../Jquery/jquery-3.6.4.min.js"></script>
+    <script type="text/javascript" src="../../DataTable/datatables.min.js"></script>
+    <?php include '../../Layout/scripts.php' ?>
+</body>
+
+</html>
