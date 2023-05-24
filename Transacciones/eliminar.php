386f46f99d14524f806b5515aff9215356eae8fc
rubenRamirez31 <l20te0221@teziutlan.tecnm.mx>
2023-05-23 23:36:41 -0600

    sip

--- Transacciones/eliminar.php
+++ Transacciones/eliminar.php
@@ -1,16 +1,22 @@
 <?php
 
+//Eliminar Producto
+
+//Autor: Manuel Santos Mendo
+
+//Proteger el archivo
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require '../Conection/cn.php';
     $id = $db->real_escape_string($_POST['id']);
     $query = "DELETE FROM productos WHERE id_producto = $id ";
 
     if ($db->query($query)) {
-        header('Location: /Paginas/Admin/index.php?resul=3');
+        header('Location: /soloarte/Paginas/Admin/index.php?resul=3');
     } else {
-        echo 'error al insertar';
-        exit;
+        header('Location: /soloarte/Paginas/Admin/index.php?resul=4');
     }
 } else {
     header('Location: /soloarte');
 }
+
+//Fecha de creacion: 13/05/2023
